06ee8e60c2f2e9b7f15057e77d9f0c03bb3bdec5
SunHS07 <dhiguera09@gmail.com>
2020-07-02 16:08:47 -0500

    Ajuste estilo btn

--- resources/views/principalUsuario.blade.php
+++ resources/views/principalUsuario.blade.php
@@ -70,7 +70,7 @@
                             <div class="d-flex flex-row">
                                 @if (!empty($tableros))
                                 <div class="p-2 bd-highlight" id="actividades">
-                                    <button id="actividades" data-toggle="modal" data-target="#createActividad"> 
+                                    <button id="actividades" class="actividades center-block" data-toggle="modal" data-target="#createActividad" style="width: fit-content;"> 
                                         <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-plus-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                             <path fill-rule="evenodd" d="M8 3.5a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5H4a.5.5 0 0 1 0-1h3.5V4a.5.5 0 0 1 .5-.5z"/>
                                             <path fill-rule="evenodd" d="M7.5 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0V8z"/>
